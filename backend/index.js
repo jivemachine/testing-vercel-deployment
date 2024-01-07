e3a025c3518b45c18b228d041a4335a2d1a5802b
Jeremy <the.jeremy.cobb@gmail.com>
2024-01-07 17:56:52 -0600

    added index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,51 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
-app.use("/", (req, res) => {
-    res.send("Server is running.");
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+require("dotenv").config();
+
+const CHAT_ENGINE_PROJECT_ID = process.env.PROJECT_KEY;
+const CHAT_ENGINE_PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+app.post("/signup", async (req, res) => {
+  const { username, secret, email, first_name, last_name } = req.body;
+
+  // Store a user-copy on Chat Engine!
+  // Docs at rest.chatengine.io
+  try {
+    const r = await axios.post(
+      "https://api.chatengine.io/users/",
+      { username, secret, email, first_name, last_name },
+      { headers: { "Private-Key": CHAT_ENGINE_PRIVATE_KEY } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
-app.listen(5001, console.log("Server started PORT 5001"));
+app.post("/login", async (req, res) => {
+  const { username, secret } = req.body;
+
+  // Fetch this user from Chat Engine in this project!
+  // Docs at rest.chatengine.io
+  try {
+    const r = await axios.get("https://api.chatengine.io/users/me/", {
+      headers: {
+        "Project-ID": CHAT_ENGINE_PROJECT_ID,
+        "User-Name": username,
+        "User-Secret": secret,
+      },
+    });
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+// vvv On port 3001!
+app.listen(3001);
