3a33313ca4f40c6e68148b3b89f0b95251d5f438
Jeremy <the.jeremy.cobb@gmail.com>
2024-01-07 19:22:21 -0600

    added vercel.json again

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const axios = require("axios");
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: true }));
 
 require("dotenv").config();
